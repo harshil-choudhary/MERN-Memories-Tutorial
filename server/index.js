0f46bff4faf8ecd28d9628941ebfa9a077800db4
harshilchoudhary <harshilchoudharyids@gmail.com>
2021-08-09 10:00:33 +0530

    Redux config

--- server/index.js
+++ server/index.js
@@ -7,11 +7,10 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended : true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended : true}));
 app.use(cors());
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://harshilchoudhary:rq6XwJJRvc9lb8EC@cluster0.fy1pw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
